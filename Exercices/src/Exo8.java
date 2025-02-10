446e4bf717d3240c169ee69b61e7654bca641a6c
Manille Desseaux <manille024@gmail.com>
2025-02-10 14:27:35 +0100

    exo8

---
+++ Exercices/src/Exo8.java
@@ -0,0 +1,8 @@
+public class Exo8 {
+    public static void main(String[] args) {
+        char[] myArray42 = {'Q', 'u', 'a', 'r', 'a', 'n', 't', 'e', '-', 'd', 'e', 'u', 'x'};
+
+        String myArray42Word = String.valueOf(myArray42);
+        System.out.println(myArray42Word + " La grande réponse sur la vie, l'univers et le reste !");
+    }
+}
