28385d3f4d39d464fcd7d32443bc62b07cbc3d15
Manille Desseaux <manille024@gmail.com>
2025-02-10 14:07:51 +0100

    exo5

---
+++ Exercices/src/Exo5.java
@@ -0,0 +1,8 @@
+public class Exo5 {
+    public static void main(String[] args) {
+        String variable = "Je suis une variable !";
+
+        // int variableCheck = variable;
+        // System.out.println( variableCheck);
+    }
+}
