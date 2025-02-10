b47171dd0d0844a6c7c54b499c1e413107b40d0a
Manille Desseaux <manille024@gmail.com>
2025-02-10 13:58:14 +0100

    exo4

---
+++ Exercices/src/Exo4.java
@@ -0,0 +1,8 @@
+public class Exo4 {
+    public static void main(String[] args) {
+        String my42count= "quarante-deux";
+
+        int my42countLength = my42count.length();
+        System.out.println( my42countLength);
+    }
+}
