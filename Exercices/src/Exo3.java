2e57bf0a98c5c62a67461473c2cf7e795e4a8fc1
Manille Desseaux <manille024@gmail.com>
2025-02-10 13:50:56 +0100

    exo3

---
+++ Exercices/src/Exo3.java
@@ -0,0 +1,17 @@
+public class Exo3 {
+
+    //variables primitifs
+    byte petit_nombre = 127;
+    short moyen_nombre = 32767;
+    int nombre = 1;
+    long grd_nb = 200000;
+    float decimal = 2;
+    double grd_decimal = 3.22;
+    char lettre = 0x63;
+    boolean true_false = true;
+
+    //variables référentiel
+    String mot = "chaine de caractère";
+    int[] tab_nombres = {10, 20, 30, 40};
+
+}
