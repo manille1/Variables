2e9e4ea7d9fc6fc822106fcca111e3b47230a856
Manille Desseaux <manille024@gmail.com>
2025-02-10 14:21:17 +0100

    exo7

---
+++ Exercices/src/Exo7.java
@@ -0,0 +1,8 @@
+public class Exo7 {
+    public static void main(String[] args) {
+        char[] myArray42 = {'q', 'u', 'a', 'r', 'a', 'n', 't', 'e', '-', 'd', 'e', 'u', 'x'};
+
+        int myArray42Length = myArray42.length;
+        System.out.println(myArray42Length);
+    }
+}
